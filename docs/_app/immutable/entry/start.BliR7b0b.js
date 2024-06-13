679b665b6fd4c6c2c2676cd334ea7ddd2f20502c
Hongbin Miao <zslrmhb@gmail.com>
2024-06-13 16:41:22 -0700

    update github pages

---
+++ docs/_app/immutable/entry/start.BliR7b0b.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.HarVaXw7.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div>
   <App />
 </div>
-<!-- 
+
 <style>
   :global(body) {
     margin: 0;
@@ -17,4 +17,4 @@
     background: rgb(13, 19, 32);
     background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
   }
-</style> -->
+</style>
