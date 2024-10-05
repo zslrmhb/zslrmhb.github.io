5a6b8e037b32379a1a4cf80d0a7d8ac033710ee9
Hongbin Miao <zslrmhb@gmail.com>
2024-10-04 17:14:56 -0700

    update github pages

--- src/components/contents/Navbar.svelte
+++ src/components/contents/Navbar.svelte
@@ -3,7 +3,7 @@
 
 </script>
 
-<div class="pointer-events-none inset-x-0 mx-auto mb-7 flex h-full max-h-14 text-4xl font-bold">
+<div class="pointer-events-none inset-x-0 mx-auto mb-7 flex h-full max-h-14 font-bold text-2xl sm:text-3xl xl:text-4xl">
     <!-- <div class="fixed inset-x-0 top-0 h-16 w-full bg-background to-transparent backdrop-blur-lg [-webkit-mask-image:linear-gradient(to_top,black,transparent)] dark:bg-background"></div> -->
     <div class="pointer-events-auto relative mx-auto flex h-full min-h-full items-center gap-12 rounded-full bg-background px-1 [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05)">
     {#each PERSONAL_INFO.navbar as item}
