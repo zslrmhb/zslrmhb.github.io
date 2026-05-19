290dcd50e2829decc51eb622995212b6a283ced9
Hongbin Miao <zslrmhb@gmail.com>
2026-05-18 19:16:20 -0700

    update github pages

--- src/lib/components/portfolio/About.svelte
+++ src/lib/components/portfolio/About.svelte
@@ -9,11 +9,7 @@
 	</BlurFade>
 	<BlurFade delay={BLUR_FADE_DELAY * 1.4}>
 		<div class="prose max-w-full font-sans text-sm text-pretty text-gray-500 text-muted-foreground">
-			<p>My name is Hongbin Miao (缪鸿彬) and I am a graduate student at UC Irvine.</p>
-			<p>
-				I am interested in <b>machine learning</b>, <b>software engineering</b>, and
-				<b>data science</b>.
-			</p>
+			<p>My name is Hongbin Miao (缪鸿彬)</p>
 			<p>
 				I love hands-on and intuitive learning from <b>first principle</b> and want to communicate my
 				learning through my projects (and hopefully through my blogs) 😃!
@@ -23,15 +19,15 @@
                 <a href="https://www.amazon.com/AI-Engineering-Building-Applications-Foundation/dp/1098166302"><u>AI Engineering</u></a>,
                 <a href="https://mml-book.github.io"><u>Mathematics for Machine Learning</u></a> 
             </p> -->
-			<p>
+			<!-- <p>
 				Research Interest:
 				<a
 					href="https://github.com/leofan90/Awesome-World-Models?tab=readme-ov-file#foundation-paper-of-world-model"
-					><u>World Models</u></a
+					><u>World Models & Self-Supervised Learning</u></a
 				>,
 				<a href="https://transformer-circuits.pub"><u>Mechanistic Interpretability</u></a>,
 				<a href="https://youtu.be/gEbbGyNkR2U?si=UDOyahWRNr8Bu3KS"><u>Continual Learning</u></a>
-			</p>
+			</p> -->
 		</div>
 	</BlurFade>
 </section>
